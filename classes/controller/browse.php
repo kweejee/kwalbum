4875090a5ca0026bb74d69b5afde4de705e79b59
Tim Redmond <kweejee@gmail.com>
2013-10-30 21:07:19 -0600

    whitespace and comment cleanup of browse controller

--- classes/controller/browse.php
+++ classes/controller/browse.php
@@ -12,52 +12,44 @@
 
 class Controller_Browse extends Controller_Kwalbum
 {
-	public function action_index()
-	{
+    public function action_index()
+    {
 
-		//echo Kohana::debug($this);
-		if ($this->request->uri() == 'kwalbum' and
-		    ! ($this->location or $this->date or count($this->tags) > 0))
-		{
-			$this->template->content = new View('kwalbum/index');
-			return;
-		}
+        //echo Kohana::debug($this);
+        if ($this->request->uri() == 'kwalbum' and
+            !($this->location or $this->date or count($this->tags) > 0)
+        ) {
+            $this->template->content = new View('kwalbum/index');
+            return;
+        }
 
-		//echo $this->request->route->uri(array('tags' => 'a,b', 'location' => 'd'));
-		$view = new View('kwalbum/browse/index');
-		$view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number);
-		$this->template->content = $view;
-		$this->template->title = 'browsing';
+        //echo $this->request->route->uri(array('tags' => 'a,b', 'location' => 'd'));
+        $view = new View('kwalbum/browse/index');
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number);
+        $this->template->content = $view;
+        $this->template->title = 'browsing';
 
-	}
+    }
 
-	public function action_comments()
-	{
+    public function action_comments()
+    {
+        $view = new View('kwalbum/browse/comments');
+        Model_Kwalbum_Comment :: set_sort_field('create');
+        Model_Kwalbum_Comment :: set_sort_direction('DESC');
+        $view->items = Model_Kwalbum_Comment :: get_thumbnails($this->page_number);
+        $this->template->content = $view;
+        $this->template->title = 'browsing newest comments';
 
-		//echo Kohana::debug($this);
-//		if ( $this->request->uri == 'kwalbum' and ! ($this->location or $this->date or count($this->tags) > 0))
-//		{
-//			$this->template->content = new View('kwalbum/index');
-//			return;
-//		}
+    }
 
-		$view = new View('kwalbum/browse/comments');
-		Model_Kwalbum_Comment :: set_sort_field('create');
-		Model_Kwalbum_Comment :: set_sort_direction('DESC');
-		$view->items = Model_Kwalbum_Comment :: get_thumbnails($this->page_number);
-		$this->template->content = $view;
-		$this->template->title = 'browsing newest comments';
+    public function action_popular()
+    {
+        $view = new View('kwalbum/browse/popular');
+        Model_Kwalbum_Item :: set_sort_field('count');
+        Model_Kwalbum_Item :: set_sort_direction('DESC');
+        $view->items = Model_Kwalbum_Item :: get_thumbnails($this->page_number);
+        $this->template->content = $view;
+        $this->template->title = 'browsing most popular';
 
-	}
-
-	public function action_popular()
-	{
-		$view = new View('kwalbum/browse/popular');
-		Model_Kwalbum_Item :: set_sort_field('count');
-		Model_Kwalbum_Item :: set_sort_direction('DESC');
-		$view->items = Model_Kwalbum_Item :: get_thumbnails($this->page_number);
-		$this->template->content = $view;
-		$this->template->title = 'browsing most popular';
-
-	}
+    }
 }
