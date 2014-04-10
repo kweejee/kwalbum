7a7fb836dad85e57c46f1bd21d2dfe0c270d3ca6
Tim Redmond <kweejee@gmail.com>
2014-04-09 21:19:14 -0600

    add ability to browse specific permission levels

--- classes/controller/browse.php
+++ classes/controller/browse.php
@@ -3,7 +3,7 @@
  *
  *
  * @author Tim Redmond <kweejee@tummycaching.com>
- * @copyright Copyright 2009-2012 Tim Redmond
+ * @copyright Copyright 2009-2014 Tim Redmond
  * @license GNU General Public License version 3 <http://www.gnu.org/licenses/>
  * @package kwalbum
  * @since Sep 1, 2009
@@ -88,17 +88,16 @@ class Controller_Browse extends Controller_Kwalbum
 
             }
 
-		$tags = explode(',', htmlspecialchars(@ $_POST['tags']));
-		for ($i = 0; $i < count($tags); $i++) {
-			$tags[$i] = trim($tags[$i]);
-		}
-		$item->tags = $tags;
+            $tags = explode(',', htmlspecialchars(@ $_POST['tags']));
+            for ($i = 0; $i < count($tags); $i++) {
+                $tags[$i] = trim($tags[$i]);
+            }
+            $item->tags = $tags;
         }
     }
 
     public function action_index()
     {
-
         //echo Kohana::debug($this);
         if ($this->request->uri() == 'kwalbum' and
             !($this->location or $this->date or count($this->tags) > 0)
@@ -112,15 +111,14 @@ class Controller_Browse extends Controller_Kwalbum
         $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
         $this->template->content = $view;
         $this->template->title = 'browsing';
-
     }
 
     public function action_comments()
     {
         $view = new View('kwalbum/browse/comments');
-        Model_Kwalbum_Comment :: set_sort_field('create');
-        Model_Kwalbum_Comment :: set_sort_direction('DESC');
-        $view->items = Model_Kwalbum_Comment :: get_thumbnails($this->page_number);
+        Model_Kwalbum_Comment::set_sort_field('create');
+        Model_Kwalbum_Comment::set_sort_direction('DESC');
+        $view->items = Model_Kwalbum_Comment::get_thumbnails($this->page_number, $this->in_edit_mode);
         $this->template->content = $view;
         $this->template->title = 'browsing newest comments';
 
@@ -129,11 +127,55 @@ class Controller_Browse extends Controller_Kwalbum
     public function action_popular()
     {
         $view = new View('kwalbum/browse/popular');
-        Model_Kwalbum_Item :: set_sort_field('count');
-        Model_Kwalbum_Item :: set_sort_direction('DESC');
-        $view->items = Model_Kwalbum_Item :: get_thumbnails($this->page_number);
+        Model_Kwalbum_Item::set_sort_field('count');
+        Model_Kwalbum_Item::set_sort_direction('DESC');
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
         $this->template->content = $view;
         $this->template->title = 'browsing most popular';
+    }
 
+    public function action_public()
+    {
+        $view = new View('kwalbum/browse/index');
+        Model_Kwalbum_Item::append_where('hide_level', 0);
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
+        $this->template->content = $view;
+        $this->template->title = 'browsing public permissioned';
+    }
+
+    public function action_member()
+    {
+        $view = new View('kwalbum/browse/index');
+        Model_Kwalbum_Item::append_where('hide_level', 1);
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
+        $this->template->content = $view;
+        $this->template->title = 'browsing member permissioned';
+    }
+
+    public function action_privileged()
+    {
+        $view = new View('kwalbum/browse/index');
+        Model_Kwalbum_Item::append_where('hide_level', 2);
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
+        $this->template->content = $view;
+        $this->template->title = 'browsing privileged permissioned';
+    }
+
+    public function action_contributor()
+    {
+        $view = new View('kwalbum/browse/index');
+        Model_Kwalbum_Item::append_where('hide_level', 3);
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
+        $this->template->content = $view;
+        $this->template->title = 'browsing contributor permissioned';
+    }
+
+    public function action_admin()
+    {
+        $view = new View('kwalbum/browse/index');
+        Model_Kwalbum_Item::append_where('hide_level', 5);
+        $view->items = Model_Kwalbum_Item::get_thumbnails($this->page_number, $this->in_edit_mode);
+        $this->template->content = $view;
+        $this->template->title = 'browsing admin permissioned';
     }
 }
