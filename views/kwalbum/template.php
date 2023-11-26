5d73efc677e0360947396f629c0c1e97bb581178
Tim Redmond <kweejee@gmail.com>
2023-11-26 00:08:08 -0700

    Change CRLF to LF in views that weren't LF already

--- views/kwalbum/template.php
+++ views/kwalbum/template.php
@@ -6,7 +6,6 @@
  * @author Tim Redmond <kweejee@tummycaching.com>
  * @copyright Copyright 2009-2012 Tim Redmond
  * @license GNU General Public License version 3 <http://www.gnu.org/licenses/>
- * @version 3.0 Jul 1, 2009
  * @package kwalbum
  * @since 3.0 Jul 1, 2009
  */
